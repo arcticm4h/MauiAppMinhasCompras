af5063ff52c452cb9b3e6dd5e37455405eac3198
Maria Eduarda Rodrigues de Almeida <mariar.almeida478@gmail.com>
2026-03-30 07:41:36 -0300

    Atualização da interface

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,24 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async Task ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco_unitario.Text)
+			};
+
+			await App.Db.Insert(p);
+			await DisplayAlertAsync("Sucesso", "Registro inserido", "Ok");
+
+		} catch(Exception ex)
+		{
+			await DisplayAlertAsync("Ops", ex.Message, "Ok");
+		}
+    }
 }
